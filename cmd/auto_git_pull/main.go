71eb3d12bb2168b06e3e84ccc5178f08cf318448
qiheimiman <qiheimiman@gmail.com>
2025-08-07 23:11:31 +0800

    docs: 更新webhook接收事件的注释

--- cmd/auto_git_pull/main.go
+++ cmd/auto_git_pull/main.go
@@ -9,7 +9,7 @@ import (
 	"os/exec"
 )
 
-// webhook 接收 GitHub 推送事件 测试1
+// webhook 接收 GitHub 推送事件 测试12
 func main() {
 	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
