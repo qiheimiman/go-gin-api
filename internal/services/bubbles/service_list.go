63c4bee880a02f7b9d68974b66e1b910268d6942
qiheimiman <qiheimiman@gmail.com>
2025-08-10 21:03:06 +0800

    fix(bubbles): 修正SQL查询中rank字段的引号格式

--- internal/services/bubbles/service_list.go
+++ internal/services/bubbles/service_list.go
@@ -7,7 +7,7 @@ import (
 
 func (s *service) List(ctx core.Context) (info []*bubbles1000.Bubbles1000, err error) {
 
-	err = s.db.GetDbR().Table("bubbles1000").Where("'rank' <= ?", 30).Find(&info).Error
+	err = s.db.GetDbR().Table("bubbles1000").Where("`rank` <= ?", 30).Find(&info).Error
 	if err != nil {
 		return nil, err
 	}
